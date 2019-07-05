cdc2ece6ff8cd8c8f45aeb1d2e59a39c65f8f104
A.SebastianG <a.sebastiang@d-ans4dev-cz-1.cz.moravia-it.com>
2019-07-05 16:26:32 +0200

    adding script files

---
+++ tmux_secman.sh
@@ -0,0 +1,24 @@
+tmux list-session > ./tmux_session_list.dat
+clear
+if [ -s tmux_session_list.dat ]; then
+    lines="./tmux_session_list.dat"
+    unattached=false
+    while IFS= read -r line
+    do
+      if [[ ! $line == *"attached"* ]]; then
+        echo "Unattached session found..."
+	unattached=true
+      fi
+    done < "$lines"
+
+    if ! $unattached; then
+	#No session found, creating one in detached mode
+	tmux new-session -d
+    fi
+else
+    #No session found, creating one in detached mode
+    echo 'No tmux session found, creating one...'
+    tmux new-session -d
+fi
+rm ./tmux_session_list.dat
+#At this point Script exit with at least one unattached session available, run "tmux attach" to join
